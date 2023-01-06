68d5027c4727980460b54114c2224cee43a1478f
OSCAR JOSE DURAN CUELLO <odurancuello@gmail.com>
2023-01-05 23:15:43 -0400

    Add project files.

---
+++ Practica/Estudiante.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica
+{
+    public class Estudiante
+    {
+        public string carrera { get; set; }
+        public List<string> calificaciones { get; set; }
+        public List<string> cursando { get; set; }
+        public int creditosActuales { get; set; }
+
+        public Estudiante(string carrera)
+        {
+            this.carrera = carrera;
+            this.calificaciones = new List<string>();
+            this.cursando = new List<string>();
+            this.creditosActuales = 0;
+        }
+
+        public List<string> Inscribir(string asignatura)
+        {
+            if (!cursando.Contains(asignatura))
+            {
+                cursando.Add(asignatura);
+                return cursando;
+            }
+            else
+            {
+                throw new Exception("Asignatura ya inscrita");
+            }
+        }
+
+        public List<string> Retirar(string asignatura)
+        {
+            var itemToRemove = cursando.Single(r => r.Equals(asignatura));
+            cursando.Remove(itemToRemove);
+            return cursando;
+        }
+
+        public static bool Pasar(float calificacion)
+        {
+            if (calificacion > 69)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ TestProjectPractica/UnitTest1.cs
@@ -0,0 +1,38 @@
+using Practica;
+
+namespace TestProjectPractica
+{
+    public class UnitTest1
+    {
+        [Theory]
+        [InlineData(69.5)]
+        [InlineData(70)]
+        [InlineData(82)]
+        [InlineData(95)]
+        public void Pasar_True(float nota)
+        {
+            Assert.True(Estudiante.Pasar(nota));
+        }
+
+        [Theory]
+        [InlineData(69)]
+        [InlineData(10)]
+        [InlineData(50)]
+        [InlineData(25)]
+        public void Pasar_False(float nota)
+        {
+            Assert.False(Estudiante.Pasar(nota));
+        }
+
+        [Fact]
+        public void Inscribir_Una_Materia_Dos_Veces()
+        {
+            Estudiante estudiante = new Estudiante("Ingeniería de Software");
+            string materia1 = "Construcción de Software";
+            string materia2 = "Laboratorio Construcción de Software";
+            estudiante.Inscribir(materia1);
+            estudiante.Inscribir(materia2);
+            Assert.Throws<Exception>(() => estudiante.Inscribir(materia1));
+        }
+    }
+}
