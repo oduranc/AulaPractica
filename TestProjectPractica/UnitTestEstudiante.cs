4cf6ff121f66ee78ca5e62422e31177a5b99a9db
OSCAR JOSE DURAN CUELLO <odurancuello@gmail.com>
2023-01-08 13:52:15 -0400

    Add tests to Profesor and remove UnitTest1

--- TestProjectPractica/UnitTestEstudiante.cs
+++ TestProjectPractica/UnitTestEstudiante.cs
@@ -2,7 +2,7 @@ using Practica;
 
 namespace TestProjectPractica
 {
-    public class UnitTest1
+    public class UnitTestEstudiante
     {
         // Arrange
         [Theory]
@@ -12,10 +12,13 @@ namespace TestProjectPractica
         [InlineData(95)]
         public void Pasar_True(float nota)
         {
+            // Act
+            bool pasar = Estudiante.Pasar(nota);
             // Assert
-            Assert.True(Estudiante.Pasar(nota));
+            Assert.True(pasar);
         }
 
+        // Arrange
         [Theory]
         [InlineData(69)]
         [InlineData(10)]
@@ -23,18 +26,39 @@ namespace TestProjectPractica
         [InlineData(25)]
         public void Pasar_False(float nota)
         {
-            Assert.False(Estudiante.Pasar(nota));
+            // Act
+            bool pasar = Estudiante.Pasar(nota);
+            // Assert
+            Assert.False(pasar);
         }
 
         [Fact]
         public void Inscribir_Una_Materia_Dos_Veces()
         {
+            // Arrange
             Estudiante estudiante = new Estudiante("Ingeniería de Software");
             string materia1 = "Construcción de Software";
             string materia2 = "Laboratorio Construcción de Software";
+            // Act
             estudiante.Inscribir(materia1);
             estudiante.Inscribir(materia2);
+            // Assert
             Assert.Throws<Exception>(() => estudiante.Inscribir(materia1));
+        }
+
+        [Fact]
+        public void Inscribir_Solo_Una_Vez()
+        {
+            // Arrange
+            Estudiante estudiante = new Estudiante("Ingeniería de Software");
+            string materia1 = "Desarrollo de Software III";
+            string materia2 = "Laboratorio Desarrollo de Software III";
+            // Act
+            estudiante.Inscribir(materia1);
+            estudiante.Inscribir(materia2);
+            int lenght = estudiante.cursando.Count();
+            // Assert
+            Assert.Equal(2, lenght);
         }
     }
 }
